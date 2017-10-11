966be6e435c9796f18ee79696a8f77902afba4a2
josephjripple <josephjripple@gmail.com>
2017-10-11 08:26:25 -0400

    reconfigure code

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,139 +1,146 @@
 (function() {
     function SongPlayer(Fixtures) {
-         var SongPlayer = {};
+        var SongPlayer = {};
 
-         /**
-         * @desc Store current chosen album
+        // PRIVATE ATTRIBUTES
+
+        /**
+         * @desc Store the information of the currently chosen album
          * @type {Object}
          */
-         var currentAlbum = Fixtures.getAlbum();
+        var currentAlbum = Fixtures.getAlbum();
 
-         /**
+        /**
          * @desc Buzz object audio file
          * @type {Object}
          */
-         var currentBuzzObject = null;
+        var currentBuzzObject = null;
 
-         /**
+        // PRIVATE FUNCTIONS
+
+        /**
          * @function setSong
          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
          * @param {Object} song
          */
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            }
 
-         var setSong = function(song) {
-           if (currentBuzzObject) {
-             currentBuzzObject.stop();
-             SongPlayer.currentSong.playing = null;
-           }
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
 
-           currentBuzzObject = new buzz.sound(song.audioUrl, {
-              formats: ['mp3'],
-              preload: true
-           });
+            SongPlayer.currentSong = song;
+        };
 
-           SongPlayer.currentSong = song;
-         };
-
-         /**
+        /**
          * @function playSong
-         * @desc Plays the currentBuzzObject and sets song property to true
+         * @desc Plays the audio file of currentBuzzObject and changes the song's status to playing
          * @param {Object} song
          */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
 
-         var playSong = function(song) {
-           currentBuzzObject.play();
-           song.playing = true;
-         };
-
-         /**
+        /**
          * @function stopSong
-         * @desc Stops the song from playing and removes from play status
+         * @desc Stops the audio file of currentBuzzObject and removes the song's playing status
          * @param {Object} song
          */
-         var stopSong = function(song) {
+        var stopSong = function(song) {
             currentBuzzObject.stop();
             song.playing = null;
-         };
+        };
 
-         /**
+        /**
          * @function getSongIndex
-         * @desc Get the index of selected song from array on current albumctrl
+         * @desc Get the index of the chosen song from the array of songs on the current album
          * @param {Object} song
          */
-         var getSongIndex = function(song) {
-           return currentAlbum.songs.indexOf(song);
-         };
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
 
-         SongPlayer.currentSong = null;
+        // PUBLIC ATTRIBUTES
 
-         /**
+        /**
+         * @desc Active song object from list of songs
+         * @type {Object}
+         */
+        SongPlayer.currentSong = null;
+
+        // PUBLIC FUNCTIONS
+
+        /**
          * @function SongPlayer.play
-         * @desc Plays currentSong from beginning or paused state
+         * @desc Play current or new song
          * @param {Object} song
          */
-
-         SongPlayer.play = function(song) {
-           song = song || SongPlayer.currentSong;
+        SongPlayer.play = function(song) {
+            song = song || SongPlayer.currentSong;
             if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
+            } else if (SongPlayer.currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    playSong(song);
+                }
+            }
+        };
 
-         } else if (SongPlayer.currentSong === song) {
-              if (currentBuzzObject.isPaused()) {
-                  playSong(song);
-              }
-         }
-      };
-
-      /**
-      * @function SongPlayer.pause
-      * @desc pauses current song
-      * @param {Object} song
-      */
-
+        /**
+         * @function SongPlayer.pause
+         * @desc Pause current song
+         * @param {Object} song
+         */
         SongPlayer.pause = function(song) {
-          song = song || SongPlayer.currentSong;
-          currentBuzzObject.pause();
-          song.playing = false;
+            song = song || SongPlayer.currentSong;
+            currentBuzzObject.pause();
+            song.playing = false;
         };
 
         /**
-        * @function SongPlayer.previous
-        * @desc Skips to previous song to play
-        * @param {Object} song
-        */
+         * @function SongPlayer.previous
+         * @desc Stop the currently playing song and play the previous song (function does not cycle, so there is no prevous song for the first song -- music will simply stop)
+         * @param {Object} song
+         */
         SongPlayer.previous = function() {
-          var currentSongIndex = getSongIndex(SongPlayer.currentSong);
-          currentSongIndex--;
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
 
-          if (currentSongIndex < 0) {
-            stopSong(SongPlayer.currentSong);
-          } else {
-              var song = currentAlbum.songs[currentSongIndex];
-              setSong(song);
-              playSong(song);
-          }
+            if (currentSongIndex < 0) {
+                stopSong(SongPlayer.currentSong);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
 
         /**
-        * @function SongPlayer.next
-        * @desc Skips to nexst song to play
-        * @param {Object} song
-        */
+         * @function SongPlayer.next
+         * @desc Stop the currently playing song and play the next song (function does not cycle, so there is no next song for the last song -- music will simply stop)
+         * @param {Object} song
+         */
         SongPlayer.next = function() {
-          var currentSongIndex = getSongIndex(SongPlayer.currentSong);
-          currentSongIndex++;
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
 
-          if (currentSongIndex > currentAlbum.songs.length - 1) {
-              stopSong(SongPlayer.currentsong);
-          } else {
-              var song = currentAlbum.songs[currentSongIndex];
-              setSong(song);
-              playSong(song);
-          }
+            if (currentSongIndex > currentAlbum.songs.length - 1) {
+                stopSong(SongPlayer.currentSong);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
 
-         return SongPlayer;
+        return SongPlayer;
     }
 
     angular
